98259c65cfdb6a7f9300a9bab3c42b9ac21740d9
Jason-Black <71369201+Jason-Black@users.noreply.github.com>
2024-08-28 16:57:32 +0000

    add preload='auto' to video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,6 +12,7 @@ const Hero = () => {
         loop
         muted
         playsInline
+        preload='auto'
       />
       <div className='drop-shadow-sm stroke-black w-full h-[90%] flex flex-col justify-center items-center text-white px-4 text-center'>
         <h1>Decentralized</h1>
